cc1204f9c43a448b8b3340e2d1aa693d5394236e
侯青青 <18006812578@163.com>
2019-07-15 20:08:32 +0800

    ceshi

--- Learngit/Learngit/ViewController.m
+++ Learngit/Learngit/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
     UILabel * label = [[UILabel alloc]init];
+    label.text = @"test";
     
     
 }

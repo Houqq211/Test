5a088456176480bb4c859888217ed2c337a98786
侯青青 <18006812578@163.com>
2019-07-16 15:41:01 +0800

    设置button的color

--- Learngit/Learngit/ViewController.m
+++ Learngit/Learngit/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor = [UIColor redColor];
     
     [self.view addSubview:btn];
 }
